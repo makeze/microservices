ceb2b1f35104a26a48ff63aa1ebd8c08dc8bf3ab
makeze <max.gana93@gmail.com>
2021-09-30 23:48:16 +0200

    refactored services

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,6 +7,7 @@ import {ExpirationCompleteListener} from "./events/listeners/expiration-complete
 import {PaymentCreatedListener} from "./events/listeners/payment-created-listener";
 
 const start = async () => {
+    console.log('Starting ord service');
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined');
     }
