3a26c46b3bf98a4cb72ca9ded46ed9905d45df0b
makeze <max.gana93@gmail.com>
2021-09-30 23:21:08 +0200

    changed startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,10 +4,10 @@ import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
-    throw new Error('JWT_KEY must be defined');
+    throw new Error('JWT_KEY must be defined!');
   }
   if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI must be defined');
+    throw new Error('MONGO_URI must be defined!');
   }
 
   try {
