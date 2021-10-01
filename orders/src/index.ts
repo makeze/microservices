ffc919dbd030e7b3939956a4c2d7525eeb035797
makeze <max.gana93@gmail.com>
2021-10-01 02:53:42 +0200

    re-depoloy

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,7 +7,6 @@ import {ExpirationCompleteListener} from "./events/listeners/expiration-complete
 import {PaymentCreatedListener} from "./events/listeners/payment-created-listener";
 
 const start = async () => {
-    console.log('Starting ord service');
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined');
     }
